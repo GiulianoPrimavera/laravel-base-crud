9161a37b70b9199ad336ba1f179c482de90afde9
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2022-01-12 16:47:18 +0100

    adds main page structure

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,4 +6,13 @@
 
 @section('main_content')
 <h1>Questa è la pagina principale</h1>
+<div class="container">
+    <ul>
+        @foreach ($comics as $comic)
+        <li>
+            {{ $comic->title }}
+        </li>
+        @endforeach
+    </ul>
+</div>
 @endsection
