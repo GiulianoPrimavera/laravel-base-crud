d3f33588b466ef163dbb8651257eb3981996a6f8
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2022-01-12 19:35:41 +0100

    adds comic details

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -9,5 +9,29 @@
     <h1>Questa è la pagina del singolo fumetto</h1>
 
     <h5>{{ $comic->title }}</h5>
+    <ul>
+        
+        <li>
+            {{ $comic->description }}
+        </li>
+        <li>
+            <img src="{{ $comic->thumb }}" alt="copertina fumetto {{ $comic->title }}">
+        </li>
+        <li>
+            price :${{ $comic->price }}
+        </li>
+        <li>
+            series: {{ $comic->series }}
+        </li>
+        <li>
+            sale date: {{ $comic->sale_date }}
+        </li>
+        <li>
+            type: {{ $comic->type }}
+        </li>
+    </ul>
+
+    <a href=" {{ route('home.index')}} " class="btn btn-primary">torna alla home</a>
+    <a href=" {{ route('comics.index')}} " class="btn btn-primary">torna alla pagina principale dei fumetti</a>
 </div>
 @endsection
