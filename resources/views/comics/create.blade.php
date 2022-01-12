a641067ee8b2a308e33d5c95c48f9a051927e9b1
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2022-01-12 17:31:19 +0100

    adds a create comic page

---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.base_layout')
+
+@section('title')
+    Create Comics
+@endsection
+
+@section('main_content')
+<div class="container">
+    <h1>Questa è la pagina principale della creazione dei fumetti</h1>
+
+</div>
+@endsection
