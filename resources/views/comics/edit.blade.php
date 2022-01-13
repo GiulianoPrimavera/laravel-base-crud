001d65ffa23bb33eef649ca01540d9f238b95ec8
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2022-01-13 15:29:08 +0100

    adds value for the input fields

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -15,43 +15,43 @@
         <div class="mb-3">
             {{-- titolo --}}
             <label for="title" class="form-label">Inserisci nuovo titolo</label>
-            <input type="text" class="form-control" name="title" id="title">
+            <input type="text" class="form-control" name="title" id="title" value="{{ $comicBook->title }}">
         </div>
         
         <div class="mb-3">
             {{-- descrizione --}}
             <label for="description" class="form-label">Inserisci nuovo descrizione</label>
-            <input type="text" class="form-control" name="description" id="description">
+            <input type="text" class="form-control" name="description" id="description" value="{{ $comicBook->description }}">
         </div>
 
         <div class="mb-3">
             {{-- thumb-nail --}}
             <label for="thumb" class="form-label">Inserisci nuovo thumb-nail</label>
-            <input type="text" class="form-control" name="thumb" id="thumb">
+            <input type="text" class="form-control" name="thumb" id="thumb" value="{{ $comicBook->thumb }}">
         </div>
 
         <div class="mb-3">
             {{-- prezzo --}}
             <label for="price" class="form-label">Inserisci nuovo prezzo</label>
-            <input type="text" class="form-control" name="price" id="price">
+            <input type="text" class="form-control" name="price" id="price" value="{{ $comicBook->price }}">
         </div>
 
         <div class="mb-3">
             {{-- serie --}}
             <label for="series" class="form-label">Inserisci nuovo serie</label>
-            <input type="text" class="form-control" name="series" id="series">
+            <input type="text" class="form-control" name="series" id="series" value="{{ $comicBook->series }}">
         </div>
 
         <div class="mb-3">
             {{-- data di uscita --}}
             <label for="sale_date" class="form-label">Inserisci nuovo data di uscita</label>
-            <input type="text" class="form-control" name="sale_date" id="sale_date">
+            <input type="text" class="form-control" name="sale_date" id="sale_date" value="{{ $comicBook->sale_date }}">
         </div>
 
         <div class="mb-3">
             {{-- tipo --}}
             <label for="type" class="form-label">Inserisci nuovo tipo</label>
-            <input type="text" class="form-control" name="type" id="type">
+            <input type="text" class="form-control" name="type" id="type" value="{{ $comicBook->type }}">
         </div>
 
         <div>
