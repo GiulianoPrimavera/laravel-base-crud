1e78a3461ff8bfb9aa7735978d357d70b91b3ee8
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2022-01-13 15:25:46 +0100

    cretes edit page

---
+++ resources/views/comics/edit.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.base_layout')
+
+@section('title')
+    Comic edit 
+@endsection
+
+@section('main_content')
+<div class="container">
+    <h1>Questa è la pagina dove puoi modificare un fumetto</h1>
+
+    <form action="{{ route('comics.update', $comicBook->id) }}" method="post">
+        @csrf
+
+
+        <div class="mb-3">
+            {{-- titolo --}}
+            <label for="title" class="form-label">Inserisci nuovo titolo</label>
+            <input type="text" class="form-control" name="title" id="title">
+        </div>
+        
+        <div class="mb-3">
+            {{-- descrizione --}}
+            <label for="description" class="form-label">Inserisci nuovo descrizione</label>
+            <input type="text" class="form-control" name="description" id="description">
+        </div>
+
+        <div class="mb-3">
+            {{-- thumb-nail --}}
+            <label for="thumb" class="form-label">Inserisci nuovo thumb-nail</label>
+            <input type="text" class="form-control" name="thumb" id="thumb">
+        </div>
+
+        <div class="mb-3">
+            {{-- prezzo --}}
+            <label for="price" class="form-label">Inserisci nuovo prezzo</label>
+            <input type="text" class="form-control" name="price" id="price">
+        </div>
+
+        <div class="mb-3">
+            {{-- serie --}}
+            <label for="series" class="form-label">Inserisci nuovo serie</label>
+            <input type="text" class="form-control" name="series" id="series">
+        </div>
+
+        <div class="mb-3">
+            {{-- data di uscita --}}
+            <label for="sale_date" class="form-label">Inserisci nuovo data di uscita</label>
+            <input type="text" class="form-control" name="sale_date" id="sale_date">
+        </div>
+
+        <div class="mb-3">
+            {{-- tipo --}}
+            <label for="type" class="form-label">Inserisci nuovo tipo</label>
+            <input type="text" class="form-control" name="type" id="type">
+        </div>
+
+        <div>
+            <button class="btn btn-primary" type="submit">Aggiorna</button>
+        </div>
+
+    </form>
+</div>
+@endsection
